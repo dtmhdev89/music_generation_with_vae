2797983751290145c8678133e451121c11e62879
Tran Minh Hieu Dinh <jrvn-hieu@Hieus-MacBook-Pro.local>
2025-05-06 15:49:04 +0700

    save preload data for better inference

--- configs/constant.py
+++ configs/constant.py
@@ -1,4 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv(
+    dotenv_path=os.environ.get(
+        "ENV_FILE_PATH"
+    )
+)
 
 
 class Constant:
@@ -7,4 +15,7 @@ class Constant:
         os.path.join("dataset/piano_audio_files")
     )
     PIANO_AUDIO_JSON_PATH = os.path.join(PIANO_AUDIO_PATH, "crawled_data")
-
+    PRELOAD_DATA_PATH = os.environ.get(
+        "PRELOAD_DATA_PATH",
+        os.path.join("dataset/preload_data")
+    )
--- domain/services/audio_genres_extraction.py
+++ domain/services/audio_genres_extraction.py
@@ -1,4 +1,5 @@
 import os
+import json
 from music_generation_with_vae.configs.constant import Constant
 from music_generation_with_vae.domain.services.audio_dataset_preprocess import AudioDatasetPreprocess
 
@@ -15,16 +16,55 @@ class AudioGenresExtraction:
 
         return self.__json_dir
 
-    def extract(self):
+    def extract(self, force=False):
         """Do Audio data gernes extraction"""
+        unique_genres_path = os.path.join(
+            Constant.PRELOAD_DATA_PATH,
+            "unique_genres.json"
+        )
 
-        for filename in os.listdir(self.json_dir):
-            if filename.endswith('.json'):
-                json_path = os.path.join(self.json_dir, filename)
-                genres = AudioDatasetPreprocess.load_and_get_genres(json_path)
-                self.__all_genres.extend(genres)
+        if (not force) and os.path.exists(unique_genres_path):
+            unique_genres = AudioGenresExtraction.load_data(unique_genres_path)
+        else:
+            for filename in os.listdir(self.json_dir):
+                if filename.endswith('.json'):
+                    json_path = os.path.join(self.json_dir, filename)
+                    genres = AudioDatasetPreprocess.load_and_get_genres(
+                        json_path
+                    )
+                    self.__all_genres.extend(genres)
 
-        unique_genres = set(self.__all_genres)
+            unique_genres = set(self.__all_genres)
+            AudioGenresExtraction.save_data(unique_genres, unique_genres_path)
+        
         max_genres = len(unique_genres)
 
         return unique_genres, max_genres
+
+    @staticmethod
+    def preload_data(file_path):
+        if os.path.exists(file_path):
+            return AudioGenresExtraction.load_data(file_path)
+        
+        return None
+
+    @staticmethod
+    def save_data(data, save_file_path):
+        try:
+            with open(save_file_path, "w") as f:
+                json.dump(data, f, indent=4)
+            print(f"Data successfully saved to {save_file_path}")
+        except Exception as e:
+            print(f"Error saving data to JSON file: {e}")
+
+    @staticmethod
+    def load_data(file_path):
+        try:
+            with open(file_path, "r") as f:
+                data = json.load(f)
+            print(f"Data successfully loaded from {file_path}")
+
+            return data
+        except Exception as e:
+            print(f"Error loading data from JSON file: {e}")
+            return None
